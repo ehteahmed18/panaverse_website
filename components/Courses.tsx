0418fae809bbf4bfa1b95acb76e4b110e72780e4
Ehtesham Ahmed <ehtexhamahmed2000@gmail.com>
2023-02-08 14:22:23 +0500

    project

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -32,7 +32,7 @@ export default function Course(){
         <Flex alignItems={useBreakpointValue({base:"center",md:"center"})} direction={useBreakpointValue({base:"column",sm:"column",md:"row"})}>
                 <Stack >
                     <Box width={"350px"} height={useBreakpointValue({base:"320px",md:"350px"})} bg={"lightgray"} p={"30px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                    mt={useBreakpointValue({base:"10px",sm:"15px",md:"40px"})} textAlign="center" >
+                    m={useBreakpointValue({base:"10px",sm:"15px",md:"40px"})} textAlign="center" >
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Artificial Intelligence(AI) and Deep Learning Specialization</Heading>
                         <Text pt={"15px"}>The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.</Text>
                         <Button2 href="../AI" mt={"10px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
@@ -40,7 +40,7 @@ export default function Course(){
                 </Stack>
                 <Stack >
                     <Box width={"350px"} height={useBreakpointValue({base:"360px",md:"350px"})} bg={"lightgray"} p={"30px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                    mt={["10px","20px","50px"]} textAlign="center">
+                    m={["10px","20px","50px"]} textAlign="center">
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Web 3.0 (Blockchain) and Metaverse Specialization</Heading>
                         <Text pt={"15px"} fontSize={"14px"}>This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.</Text>
                         <Button2 href="../metaverse" mt={"10px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
@@ -48,7 +48,7 @@ export default function Course(){
                 </Stack>
                 <Stack >
                     <Box width={"350px"} height={useBreakpointValue({base:"350px",md:"350px"})} bg={"lightgray"} p={"30px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                    mt={["10px","20px","50px"]} textAlign="center">
+                    m={["10px","20px","50px"]} textAlign="center">
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Cloud-Native Computing Specialization</Heading>
                         <Text pt={"60px"}>The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.</Text>
                         <Button2 href="../cnc" mt={"40px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
@@ -58,7 +58,7 @@ export default function Course(){
             <Flex direction={["column", "column", "row"]} >
                 <Stack direction="row">
                     <Box width={"350px"} height={useBreakpointValue({base:"380px",md:"350px"})} bg={"lightgray"} p={"30px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                    mt={["10px","20px","50px"]} textAlign="center">
+                    m={["10px","20px","50px"]} textAlign="center">
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Ambient Computing and IoT Specialization</Heading>
                         <Text pt={"50px"}>The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices. </Text>
                         <Button2 href="../IOT" mt={"30px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
@@ -66,7 +66,7 @@ export default function Course(){
                 </Stack>
                 <Stack direction="row">
                     <Box width={"350px"} height={useBreakpointValue({base:"370px",md:"350px"})} bg={"lightgray"} p={"20px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                        mt={["10px","20px","50px"]} textAlign="center">
+                        m={["10px","20px","50px"]} textAlign="center">
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Genomics and Bioinformatics Specialization</Heading>
                         <Text pt={"10px"} fontSize={"14px"}>Genomics is the study of the total genetic makeup of individual organisms, and how this genetic information is structured, functions, and has evolved; bioinformatics encompasses a diverse range of analytical methods and tools applied to genomic data. This Specialization focuses on performing complex bioinformatics analysis using the most essential Python libraries and applications.</Text>
                         <Button2 href="../Genomics" mt={"5px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
@@ -74,7 +74,7 @@ export default function Course(){
                 </Stack>
                 <Stack direction="row">
                     <Box width={"350px"} height={useBreakpointValue({base:"400px",md:"350px"})} bg={"lightgray"} p={"30px"} borderRadius={"3xl"} boxShadow={'0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'}
-                    mt={["10px","20px","50px"]} textAlign="center" >
+                    m={["10px","20px","50px"]} textAlign="center" >
                         <Heading fontSize={useBreakpointValue({base:"20px",md:"23px"})}>Network Programmability and Automation Specialization</Heading>
                         <Text pt={"10px"} fontSize={"14px"}> Network automation is now crucial due to new protocols, technologies, delivery models, and the requirement for enterprises to become more adaptable and agile. This course teaches network engineers how to automate systems with code using a variety of technologies and tools, including Linux, Python, APIs, and Git.</Text>
                         <Button2 href="../NPAS" mt={"20px"} bg={"light gray"} _hover={{ bg: "red.400", textColor: "white" }}>Learn More &gt;</Button2>
